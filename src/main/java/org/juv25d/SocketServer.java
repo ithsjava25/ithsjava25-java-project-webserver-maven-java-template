e0c324c1207419af63e71d3e9fc7c5e81aac167d
Adam Ottosson <143405021+addee1@users.noreply.github.com>
2026-02-10 13:47:17 +0100

    Add basic HTTP response support (#24)
    Rebased 4 commits in this PR.

--- src/main/java/org/juv25d/SocketServer.java
+++ src/main/java/org/juv25d/SocketServer.java
@@ -11,7 +11,6 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class SocketServer {
-
     private final HttpParser httpParser;
 
     public SocketServer(HttpParser httpParser) {
