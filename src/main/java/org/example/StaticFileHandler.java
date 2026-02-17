0e83b32a14b97f7fc721aaf23906d582d6c28ba1
Younes <younes.ahmad@iths.se>
2026-02-17 17:35:11 +0100

    Added staticFileHandler with binary-safe writing

--- src/main/java/org/example/StaticFileHandler.java
+++ src/main/java/org/example/StaticFileHandler.java
@@ -5,7 +5,6 @@ import org.example.http.HttpResponseBuilder;
 import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.util.Map;
 
@@ -14,25 +13,24 @@ public class StaticFileHandler {
     private byte[] fileBytes;
     private int statusCode;
 
-    //Constructor for production
+    // Constructor for production
     public StaticFileHandler() {
         WEB_ROOT = "www";
     }
 
-    //Constructor for tests, otherwise the www folder won't be seen
-    public StaticFileHandler(String webRoot){
+    // Constructor for tests, otherwise the www folder won't be seen
+    public StaticFileHandler(String webRoot) {
         WEB_ROOT = webRoot;
     }
 
     private void handleGetRequest(String uri) throws IOException {
-
         File file = new File(WEB_ROOT, uri);
-        if(file.exists()) {
+        if (file.exists()) {
             fileBytes = Files.readAllBytes(file.toPath());
             statusCode = 200;
         } else {
             File errorFile = new File(WEB_ROOT, "pageNotFound.html");
-            if(errorFile.exists()) {
+            if (errorFile.exists()) {
                 fileBytes = Files.readAllBytes(errorFile.toPath());
             } else {
                 fileBytes = "404 Not Found".getBytes();
@@ -41,16 +39,17 @@ public class StaticFileHandler {
         }
     }
 
-    public void sendGetRequest(OutputStream outputStream, String uri) throws IOException{
+    public void sendGetRequest(OutputStream outputStream, String uri) throws IOException {
         handleGetRequest(uri);
 
         HttpResponseBuilder response = new HttpResponseBuilder();
         response.setStatusCode(statusCode);
         response.setHeaders(Map.of("Content-Type", "text/html; charset=utf-8"));
         response.setBody(fileBytes);
-        PrintWriter writer = new PrintWriter(outputStream, true);
-        writer.println(response.build());
 
+        // Använder outputStream.write() direkt istället för PrintWriter,
+        //PrintWriter lägger till extra radbrytningar och fungerar dåligt med binär data!
+        outputStream.write(response.build());
+        outputStream.flush();
     }
-
 }
