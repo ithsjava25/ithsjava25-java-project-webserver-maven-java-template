b8aa660e679f879580fe464466db3c885ec98436
Oscar Nidemar <oscar.nidemar@gmail.com>
2026-02-19 11:28:23 +0100

    Minor change to ConnectionHandler to generate and set connection ID, and LoggingFilter to use the logger, ensuring connection ID is included in the logs

--- src/main/java/org/juv25d/filter/LoggingFilter.java
+++ src/main/java/org/juv25d/filter/LoggingFilter.java
@@ -2,14 +2,17 @@ package org.juv25d.filter;
 
 import org.juv25d.http.HttpRequest;
 import org.juv25d.http.HttpResponse;
+import org.juv25d.logging.ServerLogging;
 
 import java.io.IOException;
+import java.util.logging.Logger;
 
 public class LoggingFilter implements Filter {
+    private static final Logger logger = ServerLogging.getLogger();
 
     @Override
     public void doFilter(HttpRequest req, HttpResponse res, FilterChain chain) throws IOException {
-        System.out.println(req.method() + " " + req.path());
+        logger.info(req.method() + " " + req.path());
         chain.doFilter(req, res);
     }
 }
