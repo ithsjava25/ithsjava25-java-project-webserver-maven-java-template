1167331defff814b9ffe051449e58bf27c98df62
Firas M. <149446898+fmazmz@users.noreply.github.com>
2026-02-23 19:06:49 +0100

    Route forwarding server proxy (#119)
    * add ProxyRoute that will map a base route to its respective upstream host URL
    
    * register ProxyRoutes from application properties inside ConfigLoader
    
    * register ProxyPlugin for each ProxyRoute and log request
    
    * proxy request to upstream and relay the response back to the client
    
    * copy only non restricted Http headers
    
    * add HttpStatus enum to get status texts from the status code and log proxy response
    
    * handle connect and timeout exceptions and log warning message to console
    
    * dont log exception stack trace to console
    
    * add javadoc in ProxyRoute with a simple example
    
    * test handle offline upstream domain
    
    * test non exisiting upstream resource path
    
    * test successful response with JSON data
    
    * test that the upstream URL gets correctly built
    
    * dont log empty query string
    
    * test restricted headers filter from being proxied
    
    * test different proxy response scenarios
    
    * catch InterruptedException explicitly

--- src/main/java/org/juv25d/router/RouterConfig.java
+++ src/main/java/org/juv25d/router/RouterConfig.java
@@ -5,11 +5,19 @@ import org.juv25d.plugin.HealthCheckPlugin;
 import org.juv25d.plugin.MetricPlugin;
 import org.juv25d.plugin.NotFoundPlugin;
 import org.juv25d.plugin.StaticFilesPlugin;
+import org.juv25d.proxy.ProxyPlugin;
+import org.juv25d.proxy.ProxyRoute;
+import org.juv25d.util.ConfigLoader;
 
 public class RouterConfig {
 
     @Inject
     public RouterConfig(SimpleRouter router) {
+
+        for (ProxyRoute proxyRoute : ConfigLoader.getInstance().getProxyRoutes()) {
+            router.registerPlugin(proxyRoute.getBaseRoute(), new ProxyPlugin(proxyRoute));
+            router.registerPlugin(proxyRoute.getBaseRoute() + "/*", new ProxyPlugin(proxyRoute));
+        }
 
         router.registerPlugin("/metric", new MetricPlugin());
         router.registerPlugin("/health", new HealthCheckPlugin());
