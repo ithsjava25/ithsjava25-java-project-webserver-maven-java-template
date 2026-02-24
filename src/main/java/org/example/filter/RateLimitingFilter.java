261a32afd5a8d70289c1cfc1acb4958038b22bb6
Gabriela Aguirre <gvaguirres@gmail.com>
2026-02-24 12:35:41 +0100

    Add rate limiting filter implementation

---
+++ src/main/java/org/example/filter/RateLimitingFilter.java
@@ -0,0 +1,71 @@
+package org.example.filter;
+
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import org.example.http.HttpResponseBuilder;
+import org.example.httpparser.HttpRequest;
+import java.time.Duration;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Logger;
+
+/**
+ * Filter responsible for limiting the number of requests per client IP.
+ * Implements the Token Bucket algorithm using the Bucket4j library.
+ */
+
+public class RateLimitingFilter implements Filter {
+    private static final Logger logger = Logger.getLogger(RateLimitingFilter.class.getName());
+    private static final Map<String, Bucket> buckets = new ConcurrentHashMap<>();
+    private static final long capacity = 10;
+    private static final long refillTokens = 1;
+    private final Duration refillPeriod = Duration.ofSeconds(10);
+
+    @Override
+    public void init() {
+        logger.info("RateLimitingFilter initialized with capacity: " + capacity);
+    }
+
+    /**
+     * Intercepts the request and checks if the client has enough tokens.
+     */
+
+    @Override
+    public void doFilter(HttpRequest request, HttpResponseBuilder response, FilterChain chain) {
+
+        String clientIp = (String) request.getAttribute("clientIp");
+
+        Bucket bucket = buckets.computeIfAbsent(clientIp, k -> createNewBucket());
+
+        if (bucket.tryConsume(1)) {
+            chain.doFilter(request, response);
+        } else {
+            logger.warning("Limit exceeded per IP: " + clientIp);
+            //todo: cambiar 429 a la nueva configuracion del builder
+            //agregar el rate limit exceeded cuando el ip es privado como vimos en la clase??? ip 172.19
+            response.setStatusCode(429);
+            response.setBody("429 Too Many Requests: limit of requests exceeded.\n");
+        }
+    }
+
+    @Override
+    public void destroy() {
+        buckets.clear();
+    }
+
+    /**
+     * Configures a new Bucket with the specified bandwidth.
+     */
+    private Bucket createNewBucket() {
+        return Bucket.builder()
+                .addLimit(Bandwidth.builder()
+                        .capacity(capacity)
+                        .refillGreedy(refillTokens, refillPeriod)
+                        .build())
+                .build();
+    }
+
+    public void clearBuckets(){
+             buckets.clear();
+    }
+}
